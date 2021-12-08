6ee5301f65a0175e87ebf7cfc21d7a9efb8d76e5
thispath98 <l0641@naver.com>
2021-12-08 14:49:25 +0900

    15 동적 계획법 1

--- 15 동적 계획법 1/1003.py
+++ 15 동적 계획법 1/1003.py
@@ -1,16 +1,19 @@
 import sys
+
 T = int(sys.stdin.readline())
 N = [int(sys.stdin.readline()) for _ in range(T)]
 
-fibo = {
-    0: (1, 0),
-    1: (0, 1)
-}
+fibo = {0: (1, 0), 1: (0, 1)}
+
 
 def get_fibo_count(n):
     if n not in fibo.keys():
-        fibo[n] = (get_fibo_count(n - 1)[0] + get_fibo_count(n - 2)[0], get_fibo_count(n - 1)[1] + get_fibo_count(n - 2)[1])
+        fibo[n] = (
+            get_fibo_count(n - 1)[0] + get_fibo_count(n - 2)[0],
+            get_fibo_count(n - 1)[1] + get_fibo_count(n - 2)[1],
+        )
     return fibo[n]
+
 
 get_fibo_count(max(N))
 
